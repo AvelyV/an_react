877cb2d4ea796a12ab76024af82e075960f1b18a
AvelyV <avely.voore@gmail.com>
2022-04-26 13:36:58 +1200

    text for contact and about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
 
 export default function About() {
   return (
-    <div>About</div>
+    <Container>
+        <p>Kõik sai alguse
+           kui veel põhikoolis käisin ja igavusest ühel päeval riiulilt isa kaamera haarasin, et enda oskused proovile panna. Proovisin pildistada heinakõrsi, kive, päikeseloojangut, sätendavat lund - kõike mis eest ei põgenenud. Ühel hetkel aga haaras mind põnevus püüda pildile lepatriinu. See oli täiesti juhuslik, sest tegelikult olin tulnud ikka heinakõrsi pildistama. See sai minu kõige raskemaks katsumuseks, sest ta lendas muudkui ühelt kõrrelt teisele, ta oli nii väike ja ei teinud minuga üldse koostööd. Peale pikka püherdamist põllul, oi see ajas vihale ja ma olin õnnetu, et ma teda pildile ei saanud. Kuid siiski õnnestus see triinu lõpuks pildile saada ja oh siis seda rõõmu. See tunne mis mind toona valdas esineb siiani, sest iga õnnestunud fotosessioon loob minus mega tunde! See südamerahu ja loov väljendustunne, kui tulemusega rahul olen on kõige selle alus miks ma fotograafiaga tegelen. 
+           Täna vaatan seda lepatriinu fotot ja mõistan, kui ebaterav ja udune see on. Kuid too hetk olin ma ju seitsmendas taevas. 
+           Kui sa seda juttu loed siis tea, et edu võti on asjaga tegeleda, tegeleda ja tegeleda! Ära anna alla ja anna enda unistusele võimalus! Alusta kusagilt, sest väikesed sammud on ühel päeval suureks kasvanud. Poleks ma toona alustanud, siis ehk poleks ma avastanud oma suurt armastust fotograafia vastu. 
+        </p>
+        <p>
+            Aastate möödudes, ise aru saamata huvi fotograafia vastu suurenes ning tänu minu põhikooli ajaloo õpetajale, kes suunas ja julgustas mind ka inimesi pildistama, tegin ma sellega algust pildistades sõbrannasi ja tuttavaid. 
+        </p>
+        <p>
+            Proovinud aastate jooksul fotograafia erinevaid suundi, sai mulle selgeks, et minu hinge ei toida ainult erinevate ürituste, perekondade ja portreede jäädvustamine. Siit oli midagi puudu ja nii ma leidsin end fantaasia suuna pealt. See lubab mul olla hingest loominguline, jutustada fotoga lummav lugu ja mu hing on saanud taaskord laulda!
+        </p>
+    </Container>
   )
 }
--- src/components/Kontakt.jsx
+++ src/components/Kontakt.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
 
 export default function Kontakt() {
   return (
-      
-    <div>Kontakt</div>
+
+    <Container>
+        <p>
+            Aitäh, et tunned huvi minu tööde kohta!
+        </p>
+        <p>
+            Kui soovid küsida päringute, erinevate koostööde kohta või öelda lihtsalt tere, siis täida allolev vorm või kirjuta mulle otse <a href="mailto:info@anneliisvoore.ee">info@anneliisvoore.ee</a>.
+            Vastan 24h jooksul.
+        </p>
+    </Container>
   )
 }
