d35cae154a84d93d070d86430dfaca35cfcc12f6
AvelyV <avely.voore@gmail.com>
2022-08-01 09:59:41 +1200

    new images for portfolio page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import './../../style/portfolio.css';
+
 
 export default function Portfolio() {
   return (
-    <h1 className='pageTitle'>Portfolio</h1>
+    <>
+      <h1 className='pageTitle'>Portfolio</h1>
+      <div className='flex'>
+        
+      </div>
+    </>
   )
 }
