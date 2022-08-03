15af4d344a626a69e874d73dfd16539ceab4b5b7
AvelyV <avely.voore@gmail.com>
2022-08-04 08:03:38 +1200

    social media icons in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,28 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './../style/footer.css';
+import instagram_icon from './../img/instagram.png';
+import fb_icon from './../img/facebook.png';
 
 export default function Footer() {
   return (
     <div id="footer">
-        <div className='footer-links'>
-  
-      <Link to="/" className="navLink">
-        Avaleht
-      </Link>
-      <Link to="/portfoolio" className="navLink">
-        Portfoolio
-      </Link>
-      <Link to="/hinnakiri" className="navLink">
-        Hinnakiri
-      </Link>
-      <Link to="/fotograafist" className="navLink">
-        Fotograafist
-      </Link>
-      <Link to="/kontakt" className="navLink">
-        Kontakt
-      </Link>
-      <p>© 2022 all rights reserved</p>
+      <div className="footer-links">
+        <div>
+          <a
+            className='smIcons'
+            href="https://www.instagram.com/anneliisv_photography/"
+            target="_blank">
+            <img src={instagram_icon} alt="instagram icon"></img>
+          </a>
+          <a
+            className='smIcons'
+            href="https://www.facebook.com/AVoorePhotography"
+            target="_blank">
+            <img src={fb_icon} alt="ifacebook icon"></img>
+          </a>
+        </div>
+
+        <Link to="/" className="navLink">
+          Avaleht
+        </Link>
+        <Link to="/portfoolio" className="navLink">
+          Portfoolio
+        </Link>
+        <Link to="/hinnakiri" className="navLink">
+          Hinnakiri
+        </Link>
+        <Link to="/fotograafist" className="navLink">
+          Fotograafist
+        </Link>
+        <Link to="/kontakt" className="navLink">
+          Kontakt
+        </Link>
+        <p>© 2022 all rights reserved</p>
       </div>
     </div>
   );
