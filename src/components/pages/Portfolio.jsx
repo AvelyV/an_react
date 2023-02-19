f536b5fee0e3a622d0d1a62b72de8f54fa0b8198
AvelyV <avely.voore@gmail.com>
2023-02-19 13:24:42 +1300

    Remove xmas photos and add some other pictures to portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -23,24 +23,35 @@ import p19 from './../../img/portfolio/p19.jpg';
 import p20 from './../../img/portfolio/p20.JPG';
 import p21 from './../../img/portfolio/p21.JPG';
 
-import j1 from './../../img/portfolio/j1.jpg';
-import j2 from './../../img/portfolio/j2.jpg';
-import j3 from './../../img/portfolio/j3.jpg';
-import j4 from './../../img/portfolio/j4.jpg';
-import j5 from './../../img/portfolio/j5.jpg';
-import j6 from './../../img/portfolio/j6.jpg';
-import j7 from './../../img/portfolio/j7.jpg';
-import j8 from './../../img/portfolio/j8.JPG';
-import j9 from './../../img/portfolio/j9.JPG';
-import j10 from './../../img/portfolio/j10.JPG';
-import j11 from './../../img/portfolio/j11.JPG';
+import p22 from './../../img/portfolio/p22.jpg';
+import p23 from './../../img/portfolio/p23.jpg';
+import p24 from './../../img/portfolio/p24.jpg';
+import p25 from './../../img/portfolio/p25.jpg';
+import p26 from './../../img/portfolio/p26.jpg';
+import p27 from './../../img/portfolio/p27.jpg';
+import p28 from './../../img/portfolio/p28.jpg';
+import p29 from './../../img/portfolio/p29.jpg';
+import p30 from './../../img/portfolio/p30.jpg';
+import p31 from './../../img/portfolio/p31.jpg';
+
+// import j1 from './../../img/portfolio/j1.jpg';
+// import j2 from './../../img/portfolio/j2.jpg';
+// import j3 from './../../img/portfolio/j3.jpg';
+// import j4 from './../../img/portfolio/j4.jpg';
+// import j5 from './../../img/portfolio/j5.jpg';
+// import j6 from './../../img/portfolio/j6.jpg';
+// import j7 from './../../img/portfolio/j7.jpg';
+// import j8 from './../../img/portfolio/j8.JPG';
+// import j9 from './../../img/portfolio/j9.JPG';
+// import j10 from './../../img/portfolio/j10.JPG';
+// import j11 from './../../img/portfolio/j11.JPG';
 
 export default function Portfolio() {
   return (
     <>
       <h1 className='pageTitle'>Portfolio</h1>
       <div className='flex'>
-        <img alt='xmas' src={j1} className='pImage'></img>
+        {/* <img alt='xmas' src={j1} className='pImage'></img>
         <img alt='xmas' src={j2} className='pImage'></img>
         <img alt='xmas' src={j3} className='pImage'></img>
         <img alt='xmas' src={j4} className='pImage'></img>
@@ -50,8 +61,18 @@ export default function Portfolio() {
         <img alt='xmas' src={j8} className='pImage'></img>
         <img alt='xmas' src={j9} className='pImage'></img>
         <img alt='xmas' src={j10} className='pImage'></img>
-        <img alt='xmas' src={j11} className='pImage'></img>
+        <img alt='xmas' src={j11} className='pImage'></img> */}
 
+        <img alt='girl' src={p22} className='pImage'></img>
+        <img alt='girl' src={p23} className='pImage'></img>
+        <img alt='girl' src={p24} className='pImage'></img>
+        <img alt='girl' src={p25} className='pImage'></img>
+        <img alt='girl' src={p26} className='pImage'></img>
+        <img alt='girl' src={p27} className='pImage'></img>
+        <img alt='girl' src={p28} className='pImage'></img>
+        <img alt='girl' src={p29} className='pImage'></img>
+        <img alt='girl' src={p30} className='pImage'></img>
+        <img alt='girl' src={p31} className='pImage'></img>
 
 
 
